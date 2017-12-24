d095ceb7f4270eba224ad284d393ed922f8e1f68
Josiah Markley <josiahmarkley@gmail.com>
2017-12-24 12:17:33 -0500

    Initial commit

---
+++ src/main/java/com/example/springbootpivotal/SpringbootPivotalApplication.java
@@ -0,0 +1,12 @@
+package com.example.springbootpivotal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPivotalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootPivotalApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/springbootpivotal/SpringbootPivotalApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.springbootpivotal;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootPivotalApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
