969a9e7886b1cd7012baa3b8c04d25f775e7a346
Josiah Markley <josiahmarkley@gmail.com>
2017-12-24 12:51:20 -0500

    Making a simple greeting

---
+++ src/main/java/com/example/springbootpivotal/PivotalController.java
@@ -0,0 +1,15 @@
+package com.example.springbootpivotal;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class PivotalController {
+
+    @RequestMapping("/greeting")
+    public @ResponseBody String greeting() {
+        return "Greetings Earthling!";
+    }
+
+}
